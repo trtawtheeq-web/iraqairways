5de991c7fd88ba891a1584f467cdcf489e794244
trtawtheeq-web <2.82016491e+08+trtawtheeq-web@users.noreply.github.com>
2026-08-09 22:32:43 +0000

    Replace bootstrap package with minimal bootstrap-grid.css - fix Netlify build

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./iraqi-airways.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./bootstrap-grid.css";
 import "./original-search-exact.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
